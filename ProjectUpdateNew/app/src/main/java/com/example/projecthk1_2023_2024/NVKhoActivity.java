7865fddbdd6d60dbe98303b33fb9d46af2e4b29b
BichHaoHi <21522049@gm.uit.edu.vn>
2023-11-02 15:18:52 +0700

    update UI home nvKho

---
+++ ProjectUpdateNew/app/src/main/java/com/example/projecthk1_2023_2024/NVKhoActivity.java
@@ -0,0 +1,17 @@
+package com.example.projecthk1_2023_2024;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projecthk1_2023_2024.R;
+
+public class NVKhoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_home);
+    }
+}
